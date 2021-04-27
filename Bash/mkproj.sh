81ebf9323f76ac8796d3b082eb367ceb6964cd3c
Alberto Lazari <alberto.lazari@gmail.com>
2021-04-27 20:15:16 +0200

    Added gitignore

--- Bash/mkproj.sh
+++ Bash/mkproj.sh
@@ -8,16 +8,20 @@
 # It creates a new folder containing the folders you will need and a Makefile that
 # works out-of-the-box
 
+checkFlags() {
+    
+}
+
 createDir() {
     if [[ -e "$1" ]]; then
-        return 0
+        return 1
     fi
     mkdir "$1" "$1"/{src,include}  
     cp Makefile "$1"/Makefile
     cp -r ../.vscode/ "$1"/.vscode/
-    return 1
+    return 0
 }
 
-if createDir $1; then
-    echo "cannot create directoy '$1': File already exists"
+if ! createDir $1; then
+    echo "cannot create project '$1': File already exists"
 fi
